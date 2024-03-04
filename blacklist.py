bd96804df8ae7378f1ee7309f25958222e174172
Claire <nyy1228854804@gmail.com>
2024-03-04 09:17:11 +0800

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -12,7 +12,7 @@ class BlackList(object):
     def __init__(self):
         self.__blacklistFile = os.getcwd() + "/rules/black.txt"
         self.__domainlistFile = os.getcwd() + "/rules/adblockdns.backup"
-        self.__maxTask = 2000
+        self.__maxTask = 500
 
     def GenerateDomainList(self):
         try:
